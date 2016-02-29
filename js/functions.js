9837232b1d5a03e9f304821af8ff043f066470f9
egpheel <eduardo.gil@gmail.com>
2016-02-29 13:06:04 +0000

    show/hide navbar

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,27 @@
+var isNavbarShowing = false;
+
 $(window).scroll(function() {
   var wScroll = $(this).scrollTop();
+  var offset = $('.menu').offset().top;
 
   $('.hero h1').css('transform', 'translate(0px, '+ wScroll /2 +'%)')
+
+  if (wScroll > offset) {
+    if (!isNavbarShowing) {
+      console.log('show navbar');
+      $('.menu').css({'position' : 'fixed',
+                      'top' : '0',
+                      'z-index' : '0',
+                      'margin-top' : '100px'
+                    });
+    }
+    isNavbarShowing = true;
+  } else {
+    if (isNavbarShowing) {
+      console.log('hide navbar');
+    }
+    isNavbarShowing = false;
+  };
 });
 
 $(function() {
