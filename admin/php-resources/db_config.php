c1dc9a00799b6fe23a84d9557e2c77d738a23eb4
egpheel <egpheel@users.noreply.github.com>
2016-03-16 22:52:47 +0000

    Delete db_config.php

--- admin/php-resources/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("HOST", "127.0.0.1");
-define("USER", "nextdrgm_mguser");
-define("PASS", "MarcoSergioLuisGil");
-define("DATABASE", "nextdrgm_mg-admin");
-define("COOKIE_NAME", "mgAuth");
-?>
