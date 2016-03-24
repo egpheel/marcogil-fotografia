85617305a66bd94090a2b3629c6ebde7e16041f0
egpheel <eduardo.gil@gmail.com>
2016-03-24 17:10:24 +0000

    created posts archive

--- blog/app/Http/Controllers/PagesController.php
+++ blog/app/Http/Controllers/PagesController.php
@@ -9,6 +9,52 @@ class PagesController extends Controller {
     $posts = Post::orderBy('created_at', 'desc')->paginate(5);
     $recent_posts = Post::orderBy('created_at', 'desc')->take(10)->get();
 
+    $archives = Post::latest()->get()->groupBy(function($date) {
+      switch ($date->created_at->format('n')) {
+        case 1:
+          $month = 'Janeiro';
+          break;
+        case 2:
+          $month = 'Fevereiro';
+          break;
+        case 3:
+          $month = 'Março';
+          break;
+        case 4:
+          $month = 'Abril';
+          break;
+        case 5:
+          $month = 'Maio';
+          break;
+        case 6:
+          $month = 'Junho';
+          break;
+        case 7:
+          $month = 'Julho';
+          break;
+        case 8:
+          $month = 'Agosto';
+          break;
+        case 9:
+          $month = 'Setembro';
+          break;
+        case 10:
+          $month = 'Outubro';
+          break;
+        case 11:
+          $month = 'Novembro';
+          break;
+        case 12:
+          $month = 'Dezembro';
+          break;
+
+        default:
+          break;
+      }
+
+      return $month . ' ' . $date->created_at->format('Y');
+    });
+
     foreach ($posts as $post) {
       switch ($post->created_at->month) {
         case 1:
@@ -57,7 +103,7 @@ class PagesController extends Controller {
       $post->date = $date;
     }
 
-    return view('pages.home')->with('posts', $posts)->with('recent_posts', $recent_posts);
+    return view('pages.home')->with('posts', $posts)->with(compact('recent_posts'))->with(compact('archives'));
   }
 
 }
--- blog/resources/views/pages/home.blade.php
+++ blog/resources/views/pages/home.blade.php
@@ -1,63 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--jQuery-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <!--Bootstrap-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <!--parsleyjs-->
-    <script src="/js/parsley/parsley.js"></script>
-    <script src="/js/parsley/pt-pt.js"></script>
-    <link rel="stylesheet" href="/css/parsley.css">
-    <!--infinitescroll-->
-    <script src="/js/infinitescroll/infinitescroll.js"></script>
-    <!--my CSS-->
-    <link rel="stylesheet" href="/css/layout.css">
-    <title>Marco Gil - Blog</title>
-    <!--my js-->
-    <script src="/js/functions.js"></script>
-  </head>
-  <body>
-    <div class="header">
-      <h1>Marco Gil</h1>
-      <p><em>frase bastante sexy sobre cenas e viagens e assim</em></p>
-    </div>
-    <section class="navbar">
+@extends('layout')
+
+@section('scripts')
+  <!--infinitescroll-->
+  <script src="/js/infinitescroll/infinitescroll.js"></script>
+  <!--my js-->
+  <script src="/js/functions.js"></script>
+@endsection
+
+@section('title', 'Blog')
+
+@section('content')
+  <section class="content">
+    <div class="featured">
       <hr>
-      <ul><a href="#">
-          <li>Categorias</li></a><a href="#">
-          <li>Crónicas</li></a><a href="#">
-          <li>Sobre</li></a><a href="#">
-          <li>Contacto</li></a></ul>
-      <hr>
-    </section>
-    <div class="container">@if (Session::has('success'))
-      <div role="alert" class="alert alert-success"><strong>Successo: </strong>{{ Session::get('success') }}</div>@endif
-      @if (count($errors) > 0)
-      <div role="alert" class="alert alert-danger"><strong>Erros:</strong>
-        <ul>@foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>@endforeach
-        </ul>
-      </div>@endif
-    </div>
-    <section class="content">
-      <div class="featured">
-        <hr>
-        <div class="img-wrap"><img src="/img/featured-temp.jpg" alt="Publicação em destaque"></div>
-        <hr>
-        <div class="featured-post-wrap">
-          <h2>Pelas ruas de Amesterdão</h2>
-          <p>As ruas de Amesterdão são na sua maioria compostas por bicicletas, ou veículos de duas rodas. Mas esta é uma via de carros e tram´s...tive por isso a sorte de apanhar Roger a pedalar pela vida e pela cidade.</p><a href="#" class="read-more">Ler mais</a>
-        </div>
+      <div class="img-wrap">
+        <img src="/img/featured-temp.jpg" alt="Publicação em destaque">
       </div>
-      <div class="posts-wrap">     
-        <div class="posts">
-             
-          @foreach ($posts as $post)
+      <hr>
+      <div class="featured-post-wrap">
+        <h2>Pelas ruas de Amesterdão</h2>
+        <p>As ruas de Amesterdão são na sua maioria compostas por bicicletas, ou veículos de duas rodas. Mas esta é uma via de carros e tram´s...tive por isso a sorte de apanhar Roger a pedalar pela vida e pela cidade.</p>
+        <a href="#" class="read-more">Ler mais</a>
+      </div>
+    </div>
+    <div class="posts-wrap">
+      <div class="posts">
+
+        @foreach ($posts as $post)
           <div class="post">
             <div class="post-info">
               <p class="date">
@@ -65,25 +34,51 @@
               </p>
               <h2>{{ $post->title }}</h2>
             </div>
-            <div class="post-img"><img src="/img/post-temp.jpg" alt="Publicação"></div>
+            <div class="post-img">
+              <img src="/img/post-temp.jpg" alt="Publicação">
+            </div>
             <div class="post-info">
               <p>{{ $post->body }}</p>
-              <ul><a href="#">
-                  <li>Perspectivas</li></a><a href="#">
-                  <li>Música</li></a><a href="#">
-                  <li>Viver</li></a><a href="#">
-                  <li>Viajar</li></a></ul>
+              <ul>
+                <a href="#">
+                  <li>Perspectivas</li>
+                </a>
+                <a href="#">
+                  <li>Música</li>
+                </a>
+                <a href="#">
+                  <li>Viver</li>
+                </a>
+                <a href="#">
+                  <li>Viajar</li>
+                </a>
+              </ul>
             </div>
             <hr>
-          </div>@endforeach
-          {!! $posts->render() !!}
-        </div>
-        <div class="sidebar">
-          <p>Publicações recentes</p>
-          <ul>@foreach ($recent_posts as $post)<a href="#">
-              <li>{{ $post->title }}</li></a>@endforeach</ul>
-        </div>
+          </div>
+        @endforeach
+        {!! $posts->render() !!}
       </div>
-    </section>
-  </body>
-</html>
+      <div class="sidebar">
+        <p>Publicações recentes</p>
+        <ul>
+          @foreach ($recent_posts as $post)
+            <a href="#">
+              <li>{{ $post->title }}</li>
+            </a>
+          @endforeach
+        </ul>
+        <hr>
+        <p>Arquivo</p>
+        <ul>
+          @foreach($archives as $archive=>$arc)
+            <a href="#">
+              <li>{{ $archive }}</li>
+            </a>
+          @endforeach
+        </ul>
+      </div>
+    </div>
+  </section>
+
+@endsection
