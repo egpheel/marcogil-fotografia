40534b780689f82de5aaff2a872ed2f35f4b9a45
egpheel <eduardo.gil@gmail.com>
2016-03-24 19:52:42 +0000

    created some blade partials

---
+++ blog/resources/views/partials/_header.blade.php
@@ -0,0 +1,4 @@
+<div class="header">
+  <h1>Marco Gil</h1>
+  <p><em>frase bastante sexy sobre cenas e viagens e assim</em></p>
+</div>
---
+++ blog/resources/views/partials/_navbar.blade.php
@@ -0,0 +1,18 @@
+<section class="navbar">
+  <hr>
+  <ul>
+    <a href="#">
+      <li>Categorias</li>
+    </a>
+    <a href="#">
+      <li>Crónicas</li>
+    </a>
+    <a href="#">
+      <li>Sobre</li>
+    </a>
+    <a href="#">
+      <li>Contacto</li>
+    </a>
+  </ul>
+  <hr>
+</section>
